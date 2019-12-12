3319c374065bd0ab3dbb6eb41ffda653913ac625
Jökull Sólberg Auðunsson <jokull@solberg.is>
2019-12-12 15:57:48 +0000

    Initial Commit

---
+++ LoftgaediApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoftgaediApp
+//
+//  Created by Solberg Audunsson on 12/12/2019.
+//  Copyright © 2019 Solberg Audunsson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
